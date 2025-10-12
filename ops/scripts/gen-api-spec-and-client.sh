df6a0d0404c553ae536b7714f360eb725f523548
Kyle Upton <kyleaupton@gmail.com>
2025-10-12 05:19:39 +0000

    changed name of http main file

--- ops/scripts/gen-api-spec-and-client.sh
+++ ops/scripts/gen-api-spec-and-client.sh
@@ -7,7 +7,7 @@ project_root=$(git rev-parse --show-toplevel)
 # ensure we're in the backend directory
 cd $project_root/backend
 
-swag init -g internal/http/main.go -o internal/http/docs
+swag init -g internal/http/http.go -o internal/http/docs
 
 cd $project_root/web
 npm run openapi-ts
