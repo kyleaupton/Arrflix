dd4336483bfcc8ee68042d4e0dfca38c6a840103
Kyle Upton <kyleaupton@gmail.com>
2026-01-09 01:53:40 +0000

    [seed] final name templates

--- backend/internal/db/migrations/0011_seed_dev.up.sql
+++ backend/internal/db/migrations/0011_seed_dev.up.sql
@@ -11,10 +11,10 @@ values ('Main Series Library', 'series', '/data/media/tv', true, true);
 
 -- seed name templates
 insert into name_template (name, type, template, series_show_template, series_season_template, "default")
-values ('Main Movie Template', 'movie', '{{.Media.CleanTitle}} ({{.Media.Year}}) {tmdb-{{.Media.TmdbID}}} [{{.Quality.Full}}][{{.MediaInfo.AudioCodec}} {{.MediaInfo.AudioChannels}}][{{.MediaInfo.VideoCodec}}]-{{.Release.ReleaseGroup}}', null, null, true);
+values ('Main Movie Template', 'movie', '{{.Media.CleanTitle}} ({{.Media.Year}}) {tmdb-{{.Media.TmdbID}}} {{if .Release.Edition}}{edition-{{.Release.Edition}}}{{end}} [{{.Quality.Full}}][{{.MediaInfo.AudioCodec}} {{.MediaInfo.AudioChannels}}][{{.MediaInfo.VideoCodec}}]{{if .Release.ReleaseGroup}}-{{.Release.ReleaseGroup}}{{end}}', null, null, true);
 
 insert into name_template (name, type, template, series_show_template, series_season_template, "default")
-values ('Main Series Template', 'series', '{{.Title}} - S{{.Season}}E{{.Episode}} - {{clean .EpisodeTitle}} ({{.Year}}) [{{.Quality.Full}}]', '{{.Title}} ({{.Year}})', 'Season {{.Season}}', true);
+values ('Main Series Template', 'series', '{{.Title}} - S{{.Season}}E{{.Episode}} - {{clean .EpisodeTitle}} ({{.Year}}) [{{.Quality.Full}}][{{.MediaInfo.AudioCodec}} {{.MediaInfo.AudioChannels}}][{{.MediaInfo.VideoCodec}}]{{if .Release.ReleaseGroup}}-{{.Release.ReleaseGroup}}{{end}}', '{{.Title}} ({{.Year}})', 'Season {{.Season}}', true);
 
 -- seed downloaders
 insert into downloader (name, type, protocol, url, username, password, enabled, "default")
