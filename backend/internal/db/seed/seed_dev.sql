d411aa30b9d3c535e6d0aa6aa9d9e265ca6686cb
Kyle Upton <kyleaupton@gmail.com>
2026-01-31 01:41:39 +0000

    update dev seed

--- backend/internal/db/seed/seed_dev.sql
+++ backend/internal/db/seed/seed_dev.sql
@@ -4,17 +4,17 @@ values ('dev@local.seed', 'Dev User', 'v1:bcrypt:$2a$12$n180ANBjuXfZrr.hWFZXjuki
 
 -- seed libraries
 insert into library (name, type, root_path, enabled, "default")
-values ('Main Movie Library', 'movie', '/data/media/movies', true, true);
+values ('Main Movie Library', 'movie', '/data/movies', true, true);
 
 insert into library (name, type, root_path, enabled, "default")
-values ('Main Series Library', 'series', '/data/media/tv', true, true);
+values ('Main Series Library', 'series', '/data/tv', true, true);
 
 -- seed name templates
 insert into name_template (name, type, template, series_show_template, series_season_template, "default")
 values ('Main Movie Template', 'movie', '{{.Media.CleanTitle}} ({{.Media.Year}}) {tmdb-{{.Media.TmdbID}}} {{if .Release.Edition}}{edition-{{.Release.Edition}}}{{end}} [{{.Quality.Full}}][{{.MediaInfo.AudioCodec}} {{.MediaInfo.AudioChannels}}][{{.MediaInfo.VideoCodec}}]{{if .Release.ReleaseGroup}}-{{.Release.ReleaseGroup}}{{end}}', null, null, true);
 
 insert into name_template (name, type, template, series_show_template, series_season_template, "default")
-values ('Main Series Template', 'series', '{{.Title}} - S{{.Season}}E{{.Episode}} - {{clean .EpisodeTitle}} ({{.Year}}) [{{.Quality.Full}}][{{.MediaInfo.AudioCodec}} {{.MediaInfo.AudioChannels}}][{{.MediaInfo.VideoCodec}}]{{if .Release.ReleaseGroup}}-{{.Release.ReleaseGroup}}{{end}}', '{{.Title}} ({{.Year}})', 'Season {{.Season}}', true);
+values ('Main Series Template', 'series', '{{.Media.Title}} - S{{.Media.Season}}E{{.Media.Episode}} - {{clean .Media.EpisodeTitle}} ({{.Media.Year}}) [{{.Quality.Full}}][{{.MediaInfo.AudioCodec}} {{.MediaInfo.AudioChannels}}][{{.MediaInfo.VideoCodec}}]{{if .Release.ReleaseGroup}}-{{.Release.ReleaseGroup}}{{end}}', '{{.Media.Title}} ({{.Media.Year}})', 'Season {{.Media.Season}}', true);
 
 -- seed downloaders
 insert into downloader (name, type, protocol, url, username, password, enabled, "default")
